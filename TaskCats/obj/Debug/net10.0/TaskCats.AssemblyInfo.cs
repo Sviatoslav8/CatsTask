c68204ae4fcac5854d007e10bd56dfcf2455b103
Sviatoslav8 <palamarsviatik2000@gmail.com>
2026-05-23 17:06:07 +0300

    commit

--- TaskCats/obj/Debug/net10.0/TaskCats.AssemblyInfo.cs
+++ TaskCats/obj/Debug/net10.0/TaskCats.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskCats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1263eca31b3a097dbb944a0addbe7204bf9842")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskCats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskCats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
