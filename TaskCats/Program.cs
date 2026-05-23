deb267b7075c6b48916d8a11f7c76d2f98ada28c
Sviatoslav8 <palamarsviatik2000@gmail.com>
2026-05-23 17:29:50 +0300

    commit

--- TaskCats/Program.cs
+++ TaskCats/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddDbContext<DataContext>(opt=>opt.UseSqlServer("Server=(locald
 builder.Services.AddScoped<CatService>();
 builder.Services.AddTransient<ExceptionMiddleware>();
 builder.Services.AddHostedService<HostedCat>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
@@ -23,7 +25,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //app.MapOpenApi();
 }
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
